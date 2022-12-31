3f65fb0867ed97a50f3c7975109f2d5e32164d3e
deri.indrawan <deri.indrawan@dwidasa.com>
2022-12-31 11:54:50 +0700

    externalize apiKey

--- Favgame/Core/Utils/Network/APICall.swift
+++ Favgame/Core/Utils/Network/APICall.swift
@@ -5,9 +5,24 @@
 //  Created by deri indrawan on 28/12/22.
 //
 
+import Foundation
+
 struct API {
   static let baseUrl = "https://api.rawg.io"
-  static let apiKey = "d1081332b72f464085735be4f4fecbe7"
+  
+  static var apiKey: String {
+    guard let filePath = Bundle.main.path(forResource: "Info", ofType: "plist") else {
+      fatalError("Couldn't find file 'Info.plist'.")
+    }
+    let plist = NSDictionary(contentsOfFile: filePath)
+    guard let value = plist?.object(forKey: "API_KEY") as? String else {
+      fatalError("Couldn't find key 'API_KEY' in 'Info.plist'.")
+    }
+    if (value.starts(with: "_")) {
+      fatalError("Register for a rawg.io and get an API key at https://rawg.io/apidocs")
+    }
+    return value
+  }
 }
 
 protocol Endpoint {
